f9e13858d9416b5251936d005a5d9212ddf46ee0
David Sands <contact@davidsandspictures.com>
2018-09-19 10:19:44 -0600

    Convert to MVC Design style

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,20 @@
+//
+//  DataModel.swift
+//  Todoey
+//
+//  Created by David Sands on 9/19/18.
+//  Copyright © 2018 David Sands. All rights reserved.
+//
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+    
+    
+}
